fb20f379019fb4866c440dcc83e433e19cf28a11
s2d1ent <vornfrost@gmail.com>
2021-01-18 23:38:41 +0300

    v0.1.5.1

--- scripts/main__firstlvl.js
+++ scripts/main__firstlvl.js
@@ -4,7 +4,7 @@ var copywrite=doc.querySelector(".footer__container__watermark-title");
 	copywrite.innerHTML="©s2d1ent - 2021";
 var i=0,
 	b=0;
-alert(wiw.innerWidth);
+
 doc.querySelector(".burger").addEventListener("click",()=>{
 	b=i%2;
 	if (b==0) {
